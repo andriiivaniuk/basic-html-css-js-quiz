a8915888dd5e46cf1eb9e08a79a03d5ade9cfe60
andriiivaniuk <keed888888@gmail.com>
2022-02-20 13:06:47 +0200

    bugs fixed, right answers counter added

--- script.js
+++ script.js
@@ -5,27 +5,33 @@ const Questions = {
         answers: {rightAnswer: "384.000 km", wrongAnswer0: "38.000 km", wrongAnswer1: "3.840.000 km"}},
     question3: {text: "hello?",
         answers: {rightAnswer: "no", wrongAnswer0: "yes", wrongAnswer1: "maybe"}},
-    question4: {text: "", answers: {rightAnswer: "", wrongAnswer0: "", wrongAnswer1: ""}},
-    question5: {text: "", answers: {rightAnswer: "", wrongAnswer0: "", wrongAnswer1: ""}}, 
+    question4: {text: "1",
+        answers: {rightAnswer: "1", wrongAnswer0: "2", wrongAnswer1: "3"}},
+    question5: {text: "1",
+        answers: {rightAnswer: "4", wrongAnswer0: "2", wrongAnswer1: "3"}}, 
 }
 
 let quizStarted = false;
 let curQnum = 1;
 let curAnswerInput = "";
+const questionAmount = Object.values(Questions).length;
+let rightAnswers = 0;
+let totalAnswers = 0;
+let buttons = document.getElementsByClassName("answer-button");
 
 const startQuiz = () => {
-    quizStarted ? quizStarted = true : false;
+    
+    quizStarted = true;
 
     document.getElementById("start-quiz-button").style.display = "none";
     document.getElementById("check-answer-button").style.display = "block";
 
-    let buttons = document.getElementsByClassName("answer-button");
+    
     for(but of buttons){
         but.style.visibility = "visible";
     }
 
     updateQuestion();
-
 }
 
 const updateQuestion = () => {
@@ -42,6 +48,7 @@ const updateQuestion = () => {
 
     resetButStyle();
 
+    document.getElementById("question-header").textContent = "Question: " + curQnum + " out of " + questionAmount;
     document.getElementById("question-text").textContent = Questions["question" + (curQnum)].text;
     document.getElementById("answer-button1").textContent = randArr[0];
     document.getElementById("answer-button2").textContent = randArr[1];
@@ -50,42 +57,54 @@ const updateQuestion = () => {
 }
 
 const checkAnswer = () => {
+
     if (!curAnswerInput) { return; }
 
     if (curAnswerInput === Questions["question" + (curQnum)].answers.rightAnswer){
         console.log("correct");
+        rightAnswers++;
+        totalAnswers++;
     } 
 
     else{
+        totalAnswers++;
         console.log("wrong");
     }
 
     showAnswers();
 
+    document.getElementById("right-anwers").textContent = "Right answers: " + rightAnswers + " out of " + totalAnswers;
     document.getElementById("check-answer-button").style.display = "none";
     document.getElementById("next-question-button").style.display = "block";
 
 }
 
 const inputAnswer = (buttonNum) => {
-    let buttons = document.getElementsByClassName("answer-button");
+
     for(but of buttons) { but.style.background = "white"; }
     document.getElementById("answer-button" + String(buttonNum)).style.background = "blue";
-    
     curAnswerInput = document.getElementById("answer-button" + buttonNum).textContent;
-
+    
 }
 
 const nextQuestion = () => {
+    
+    if(!quizStarted) {
+        console.log("all question looped");
+        return;
+    }
+
     curQnum++;
     curAnswerInput = "";
 
     updateQuestion();
     resetButStyle();
+
+    document.getElementById("next-question-button").style.display = "none";
 }
 
 const showAnswers = () => {
-    let buttons = document.getElementsByClassName("answer-button");
+    
     for (but of buttons){
         but.disabled = true;
         if (but.textContent === Questions["question" + (curQnum)].answers.rightAnswer){
@@ -98,10 +117,17 @@ const showAnswers = () => {
             but.style.border = "3px solid blue";
         }
     }
+
+    if (curQnum === questionAmount){   
+        quizStarted = false;
+        console.log("quiz ended");
+        document.getElementById("next-question-button").textContent = "Quiz ended";
+        return;
+    }
 }
 
 const resetButStyle = () => {
-    let buttons = document.getElementsByClassName("answer-button");
+    
     for (but of buttons){
         but.disabled = false;
         but.style.background = "white";
